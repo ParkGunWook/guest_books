5a4d84bf8aae28e4d009290e20d21da1e2de5acd
ParkGunWook <saiver852@gmail.com>
2022-01-11 18:54:29 +0900

    방명록 프로젝트 관련 초기 세팅
    모델 및 레포지토리, 뷰 템플릿 세팅 완료
    개발 환경에 필요한 환경 변수 및 도커 설정 완료
    필요 라이브러리 추가 완료

---
+++ src/test/java/com/bootcamp/guestbook/repository/GuestbookRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.bootcamp.guestbook.repository;
+
+import com.bootcamp.guestbook.entity.Guestbook;
+import com.bootcamp.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class GuestbookRepositoryTest {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummiess() {
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+            Guestbook guestbook = Guestbook.builder()
+                    .title("Title...." + i)
+                    .content("Content..." + i)
+                    .writer("user" + (i % 10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+
+    @Test
+    public void testQuery1() {
+        PageRequest gno = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, gno);
+
+        result.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void testQuery2() {
+        PageRequest gno = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+        BooleanExpression exAll = exTitle.or(exContent);
+
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L));
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, gno);
+
+        result.stream().forEach(System.out::println);
+    }
+}
