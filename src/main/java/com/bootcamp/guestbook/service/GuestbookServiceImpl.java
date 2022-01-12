b3cf746df90ba6e53319f5f72ec5e1c122744acc
ParkGunWook <saiver852@gmail.com>
2022-01-12 13:23:10 +0900

    [feat] guestbook/list 구현 #5 (#6)
    * [feat] 서비스 레이어 및 dto 구현 #5
    
    guestbook/list dto 및 서비스 구현
    
    * [feat] guestbook/list 화면 구현 #5

--- src/main/java/com/bootcamp/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/bootcamp/guestbook/service/GuestbookServiceImpl.java
@@ -1,11 +1,18 @@
 package com.bootcamp.guestbook.service;
 
 import com.bootcamp.guestbook.dto.GuestbookDTO;
+import com.bootcamp.guestbook.dto.PageRequestDTO;
+import com.bootcamp.guestbook.dto.PageResultDTO;
 import com.bootcamp.guestbook.entity.Guestbook;
 import com.bootcamp.guestbook.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -13,6 +20,17 @@ import org.springframework.stereotype.Service;
 public class GuestbookServiceImpl implements GuestbookService {
 
     private final GuestbookRepository guestbookRepository;
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = guestbookRepository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (this::entityToDto);
+
+        return new PageResultDTO<>(result, fn);
+    }
 
     @Override
     public Long register(GuestbookDTO dto) {
