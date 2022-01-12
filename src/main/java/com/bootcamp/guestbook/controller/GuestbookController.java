4e2d3d8aa9b5b0e2bd491b7d8305174dfed3124e
ParkGunWook <saiver852@gmail.com>
2022-01-12 16:33:41 +0900

    [feat] guestbook/register 구현 #7 (#8)
    guestbook/register 뷰 및 컨트롤러 구현
    guestbook/list의 모달 생성

--- src/main/java/com/bootcamp/guestbook/controller/GuestbookController.java
+++ src/main/java/com/bootcamp/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.bootcamp.guestbook.controller;
 
+import com.bootcamp.guestbook.dto.GuestbookDTO;
 import com.bootcamp.guestbook.dto.PageRequestDTO;
 import com.bootcamp.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -17,7 +20,7 @@ public class GuestbookController {
 
     private final GuestbookService guestbookService;
 
-    @GetMapping("/")
+    @GetMapping("")
     public String index() {
         return "redirect:/guestbook/list";
     }
@@ -29,5 +32,21 @@ public class GuestbookController {
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
 
         return "guestbook/list";
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        Long gno = guestbookService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
