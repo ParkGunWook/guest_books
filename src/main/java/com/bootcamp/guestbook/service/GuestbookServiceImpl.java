c1aa1a29ea6996f2f76a29959e28085eb35a5ba7
ParkGunWook <saiver852@gmail.com>
2022-01-12 16:53:57 +0900

    [feat] guestbook/read 구현 #9
    guestbook/read 뷰 ,컨트롤러, 서비스 구현

--- src/main/java/com/bootcamp/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/bootcamp/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -30,6 +31,13 @@ public class GuestbookServiceImpl implements GuestbookService {
         Function<Guestbook, GuestbookDTO> fn = (this::entityToDto);
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = guestbookRepository.findById(gno);
+
+        return result.map(this::entityToDto).orElse(null);
     }
 
     @Override
