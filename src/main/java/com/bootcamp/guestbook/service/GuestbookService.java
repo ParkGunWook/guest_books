288c309c24bcbbba956739e7c485beb0e908a1f0
ParkGunWook <saiver852@gmail.com>
2022-01-12 11:22:39 +0900

    [feat] 서비스 레이어 및 dto 구현 #5
    guestbook/list dto 및 서비스 구현

--- src/main/java/com/bootcamp/guestbook/service/GuestbookService.java
+++ src/main/java/com/bootcamp/guestbook/service/GuestbookService.java
@@ -1,10 +1,14 @@
 package com.bootcamp.guestbook.service;
 
 import com.bootcamp.guestbook.dto.GuestbookDTO;
+import com.bootcamp.guestbook.dto.PageRequestDTO;
+import com.bootcamp.guestbook.dto.PageResultDTO;
 import com.bootcamp.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto) {
         return Guestbook.builder()
@@ -12,6 +16,17 @@ public interface GuestbookService {
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .writer(dto.getWriter())
+                .build();
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity) {
+        return GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
                 .build();
     }
 }
--- src/test/java/com/bootcamp/guestbook/service/GuestbookServiceImplTest.java
+++ src/test/java/com/bootcamp/guestbook/service/GuestbookServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.bootcamp.guestbook.service;
 
 import com.bootcamp.guestbook.dto.GuestbookDTO;
+import com.bootcamp.guestbook.dto.PageRequestDTO;
+import com.bootcamp.guestbook.dto.PageResultDTO;
+import com.bootcamp.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +26,13 @@ class GuestbookServiceImplTest {
                 .build();
 
         System.out.println(guestbookService.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+        Assertions.assertNotNull(resultDTO);
     }
 }
